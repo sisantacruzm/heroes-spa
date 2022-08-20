6b180852b17f90eaf5957f4cac782d75059b3315
Sergio Santacruz <desarrollador20@smartquick.com.co>
2022-08-20 12:45:09 -0500

    Changes requerired

--- src/auth/pages/LoginPages.jsx
+++ src/auth/pages/LoginPages.jsx
@@ -9,9 +9,11 @@ export const LoginPages = () => {
 
     const onLogin = () => {
 
+        const lastPath = localStorage.getItem( 'lastPath' ) || '/'
+
         login( "Yasmin Ortega" );
 
-        navigate( "/", {
+        navigate( lastPath, {
             replace: true,
         } );
     };
